d37a40d1e0560142aa0e04676e5087128b578ca4
Vally <113294564+Needalife@users.noreply.github.com>
2024-10-18 12:15:14 +0700

    set route for getTransactionsByDate

--- backend/routes/transaction.route.js
+++ backend/routes/transaction.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
     getAllTransactions,
     getLatestTransactionsByAmount,
+    getTransactionsByDate,
 } from "../controller/transaction.controller.js";
 
 const router = express.Router();
@@ -9,5 +10,7 @@ const router = express.Router();
 router.get('/all',getAllTransactions);
 
 router.get('/latest',getLatestTransactionsByAmount);
+
+router.get('/date-range',getTransactionsByDate);
 
 export default router;
