41cba86c51a5de8bd617bf08d7a864ff4ade58d0
Vally <113294564+Needalife@users.noreply.github.com>
2024-10-20 10:28:21 +0700

    adding route for deleteTransactionById

--- backend/routes/transaction.route.js
+++ backend/routes/transaction.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+    deleteTransactionById,
     getAllTransactions,
     getLatestTransactionsByAmount,
     getTransactionsByDate,
@@ -12,5 +13,7 @@ router.get('/all', getAllTransactions);
 router.get('/latest', getLatestTransactionsByAmount);
 
 router.get('/date-range', getTransactionsByDate);
+
+router.delete('/:id',deleteTransactionById);
 
 export default router;
