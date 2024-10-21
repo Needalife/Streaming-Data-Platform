554c3ffeedb06fdc7dba3f30dedb091018d5d7ff
Vally <113294564+Needalife@users.noreply.github.com>
2024-10-21 12:52:03 +0700

    import authRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/db.js";
 import transactionRoutes from "./routes/transaction.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.use("/api/transactions", transactionRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     connectDB();
