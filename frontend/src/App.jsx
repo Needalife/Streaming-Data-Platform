292e4d50bf19f0f097f7a26a507aecfd54721368
Vally <113294564+Needalife@users.noreply.github.com>
2024-10-23 22:51:55 +0700

    route for Home?

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import SettingsPage from './pages/SettingsPage';
 import DashBoard from './pages/DashBoard';
+import HomePage from './pages/HomePage';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                     <Sidebar />
                     <Box className="flex-1 bg-gray-50">
                         <Routes>
+                            <Route path="/" element={<HomePage />} />
                             <Route path="/dashboard" element={<DashBoard />} />
                             <Route path="/settings" element={<SettingsPage />} />
                         </Routes>
