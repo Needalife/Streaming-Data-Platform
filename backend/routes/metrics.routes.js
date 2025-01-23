e2d3adc662326c011b542798925d00818e5f340d
Vally <113294564+Needalife@users.noreply.github.com>
2025-01-23 11:23:05 +0700

    delete monolith, migrate this architecture to event-driven

--- backend/routes/metrics.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAllFunctionMetrics } from '../controller/metrics.controller.js';
-
-const router = express.Router();
-
-router.get('/', getAllFunctionMetrics);
-
-export default router;
