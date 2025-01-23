7ee841c031e2400d039f5fe3c7240e50ffd231c8
Vally <113294564+Needalife@users.noreply.github.com>
2025-01-23 11:55:48 +0700

    Create db.go

---
+++ backend/event-processor/config/db.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+func ConnectMongo(uri string) *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("✅ Connected to MongoDB!")
+	return client
+}
