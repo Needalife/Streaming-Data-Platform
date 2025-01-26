e1541a026023edbed5ddc3096779f93588e61921
Vally <113294564+Needalife@users.noreply.github.com>
2025-01-26 23:46:00 +0700

    integrates with kafka

--- backend/event-processor/cmd/main.go
+++ backend/event-processor/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"event-processor/config"
+	"event-processor/events"
 	"github.com/joho/godotenv"
 	"fmt"
 	"os"
@@ -28,6 +29,24 @@ func main() {
 
 	database := client.Database("test")
 	collecion := database.Collection("test")
+	
+	kafka_host := os.Getenv("KAFKA_HOST")
+	kafka_port := os.Getenv("KAFKA_PORT")
+	if kafka_host == "" || kafka_port == "" {
+		fmt.Println("Kafka env not found")
+	}
+	
+	kafkaBrokers := []string{fmt.Sprintf("%s:%s", kafka_host, kafka_port)}
+	producer := config.ConnectKafka(kafkaBrokers)
+	if producer == nil {
+		fmt.Printf("Fail to init kafka producer \n")
+		return
+	}
+	defer producer.Close()
 
-	config.WatchChanges(collecion)
+	onChange := func(change string) {
+		events.SendMessage(producer, "mongo-changes", change)
+	}
+
+	config.WatchChanges(collecion, onChange)
 }
