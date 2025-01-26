54937247d557ec4a8388b895279eaf281feee8f9
Vally <113294564+Needalife@users.noreply.github.com>
2025-01-26 23:31:36 +0700

    add callback (onChange) for WatchChanges

--- backend/event-processor/config/db.go
+++ backend/event-processor/config/db.go
@@ -2,11 +2,12 @@ package config
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/bson"
 	"time"
-	"fmt"
 )
 
 func ConnectMongo(uri string) *mongo.Client {
@@ -22,10 +23,10 @@ func ConnectMongo(uri string) *mongo.Client {
 	return client
 }
 
-func WatchChanges(collection *mongo.Collection) {
+func WatchChanges(collection *mongo.Collection, onChange func(change string)) {
 	ctx := context.TODO()
 
-	stream, err := collection.Watch(ctx, mongo.Pipeline{})
+	stream, err := collection.Watch(ctx, mongo.Pipeline{}, options.ChangeStream().SetFullDocument(options.UpdateLookup))
 	if err != nil {
 		panic(err)
 	}
@@ -34,13 +35,17 @@ func WatchChanges(collection *mongo.Collection) {
 	fmt.Println("Watching for changes...")
 	for stream.Next(ctx) {
 		var event bson.M
-
 		if err := stream.Decode(&event); err != nil {
-			fmt.Printf("Fail to decode event %v", err)
+			fmt.Printf("Fail to decode event %v \n", err)
 			continue
 		}
 
-		fmt.Printf("Change detected: %v \n", event)
+		eventJSON, err := json.Marshal(event)
+		if err != nil {
+			fmt.Printf("Fail to marshal event: %v \n", err)
+		}
+
+		onChange(string(eventJSON))
 	}
 
 	if err := stream.Err(); err != nil {
