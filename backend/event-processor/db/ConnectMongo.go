c1c470c650936110446d6655d451fe5a84729f1c
Vally <113294564+Needalife@users.noreply.github.com>
2025-01-27 00:01:44 +0700

    split the files

---
+++ backend/event-processor/db/ConnectMongo.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+func ConnectMongo(uri string) *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
