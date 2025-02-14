1a8e9d164ac155a5c77041861aae061db562fb5b
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-14 18:35:01 +0700

    create ws.go

--- backend/gateway/internal/handlers/ws.go
+++ backend/gateway/internal/handlers/ws.go
@@ -1,1 +1,13 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func We
