9ad406d12d584afcf033ccecfe229aa19901b0d8
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-14 20:46:07 +0700

    edit fo foward connection to live-data service

--- backend/gateway/internal/handlers/ws.go
+++ backend/gateway/internal/handlers/ws.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -10,4 +11,28 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
-func We
+const liveDataWebSocketURL = "ws://livedata:8090/ws"
+
+func ForwardWSConnection(w http.ResponseWriter, r *http.Request) {
+	clientConn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error updating Websocket:", err)
+		http.Error(w, "Fail to establish ws connection", http.StatusInternalServerError)
+		return
+	}
+	defer clientConn.Close()
+
+	liveDataConn, _, err := websocket.DefaultDialer.Dial(liveDataWebSocketURL, nil)
+	if err != nil {
+		fmt.Println("Error connecting to live-data service:", err)
+		http.Error(w, "Failed to connect to live-data service", http.StatusInternalServerError)
+		return
+	}
+	defer liveDataConn.Close()
+
+	errChan := make(chan error)
+
+
+
+	
+}
