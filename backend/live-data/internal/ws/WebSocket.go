776bf8cb0d2956715642cb538f4633d2e2258659
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-18 18:43:41 +0700

    seperate aggregate connection and raw connection

--- backend/live-data/internal/ws/WebSocket.go
+++ backend/live-data/internal/ws/WebSocket.go
@@ -8,16 +8,17 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var clients = make(map[*websocket.Conn]bool)
+// WebSocket clients map
+var rawClients = make(map[*websocket.Conn]bool)
+var structuredClients = make(map[*websocket.Conn]bool)
 var mutex = sync.Mutex{}
 
-// WebSocket Upgrade
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
-// Handle WebSocket connections
-func HandleConnections(w http.ResponseWriter, r *http.Request) {
+// Handle WebSocket connections for raw data
+func HandleRawConnections(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println("WebSocket Upgrade Error:", err)
@@ -26,30 +27,68 @@ func HandleConnections(w http.ResponseWriter, r *http.Request) {
 	defer conn.Close()
 
 	mutex.Lock()
-	clients[conn] = true
+	rawClients[conn] = true
 	mutex.Unlock()
 
 	for {
 		_, _, err := conn.ReadMessage()
 		if err != nil {
 			mutex.Lock()
-			delete(clients, conn)
+			delete(rawClients, conn)
 			mutex.Unlock()
 			break
 		}
 	}
 }
 
-// Broadcast data to WebSocket clients
-func BroadcastMessage(data interface{}) {
+// Handle WebSocket connections for structured data
+func HandleStructuredConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("WebSocket Upgrade Error:", err)
+		return
+	}
+	defer conn.Close()
+
+	mutex.Lock()
+	structuredClients[conn] = true
+	mutex.Unlock()
+
+	for {
+		_, _, err := conn.ReadMessage()
+		if err != nil {
+			mutex.Lock()
+			delete(structuredClients, conn)
+			mutex.Unlock()
+			break
+		}
+	}
+}
+
+// Broadcast raw transaction data
+func BroadcastRawMessage(data interface{}) {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	for client := range clients {
+	for client := range rawClients {
 		err := client.WriteJSON(data)
 		if err != nil {
 			client.Close()
-			delete(clients, client)
+			delete(rawClients, client)
+		}
+	}
+}
+
+// Broadcast aggregated structured data
+func BroadcastStructuredData(data map[string]interface{}) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	for client := range structuredClients {
+		err := client.WriteJSON(data)
+		if err != nil {
+			client.Close()
+			delete(structuredClients, client)
 		}
 	}
 }
