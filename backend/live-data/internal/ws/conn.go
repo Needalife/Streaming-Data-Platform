635638b865ec8d1bcf713992217bb8ff724dbad8
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-18 20:01:35 +0700

    ws connection handler

---
+++ backend/live-data/internal/ws/conn.go
@@ -0,0 +1,39 @@
+package ws
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func HandleRawConnections(w http.ResponseWriter, r *http.Request) {
+	handleConnection(w, r, rawClients)
+}
+
+func HandleStructuredConnections(w http.ResponseWriter, r *http.Request) {
+	handleConnection(w, r, structuredClients)
+}
+
+func handleConnection(w http.ResponseWriter, r *http.Request, clients map[*websocket.Conn]bool) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+	    fmt.Println("WebSocket Upgrade Error:", err)
+	    return
+    }
+	defer conn.Close()
+
+	mutex.Lock()
+    clients[conn] = true
+	mutex.Unlock()
+
+	for {
+	    _, _, err := conn.ReadMessage()
+	    if err != nil {
+	        mutex.Lock()
+	        delete(clients, conn)
+	        mutex.Unlock()
+	        break
+	    }
+    }
+}
