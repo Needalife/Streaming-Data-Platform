bb9ab833f37587c5922187e88bea41814ddb1da7
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-18 20:01:23 +0700

    broadcast message logic

---
+++ backend/live-data/internal/ws/bc.go
@@ -0,0 +1,23 @@
+package ws
+
+import "github.com/gorilla/websocket"
+
+func BroadcastRawMessage(data interface{}) {
+	broadcastMessage(data, rawClients)
+}
+
+func BroadcastStructuredData(data map[string]interface{}) {
+	broadcastMessage(data, structuredClients)
+}
+
+func broadcastMessage(data interface{}, clients map[*websocket.Conn]bool) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	for client := range clients {
+	    if err := client.WriteJSON(data); err != nil {
+	        client.Close()
+	        delete(clients, client)
+	    }
+    }
+}
